7b15939f6be1a694696830ca94516ef415aff2ea
Ruslan <ruslan.bur@gmail.com>
2017-03-07 18:19:38 +0200

    Correction app config

---
+++ src/main/java/com.devruslan/ApplicationConfiguration.java
@@ -0,0 +1,14 @@
+package com.devruslan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ruslan on 07.03.17.
+ */
+@SpringBootApplication
+public class ApplicationConfiguration {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationConfiguration.class, args);
+    }
+}
